45b8d0d7951c9d889b9808c73fc6a1c73401fc33
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2024-09-28 00:30:04 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,  // Use '0.0.0.0' to allow access from external devices
-    port: 5173   // Make sure the port matches
-  }
+  // server: {
+  //   host: true,  // Use '0.0.0.0' to allow access from external devices
+  //   port: 5173   // Make sure the port matches
+  // }
 })

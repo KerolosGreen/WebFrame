15b18dd8dc60290c12bfb53b8e05434c37fe8e43
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2024-09-28 02:08:47 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: true,  // Use '0.0.0.0' to allow access from external devices
-  //   port: 5173   // Make sure the port matches
-  // }
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://valoapi.hopto.org',
+        changeOrigin: true,
+        secure: false,  // It's HTTP, so we set secure to false
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
